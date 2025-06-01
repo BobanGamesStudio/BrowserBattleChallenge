91ff56337559f2dce2cf14ac6e78dad1ab1d2361
BobanGamesStudio <boban.skolimowski@gmail.com>
2025-06-02 00:57:08 +0200

    Settings changes

--- session_auth/asgi.py
+++ session_auth/asgi.py
@@ -14,8 +14,8 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from . import routing
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'session_auth.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
 
 application = ProtocolTypeRouter({
     'http':get_asgi_application(),
